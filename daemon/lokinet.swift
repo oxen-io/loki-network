ef0972cc7788f2399d15cf78bf5da1a732baaeee
jeff <jeff@oxen.io>
2021-06-19 08:35:21 -0400

    more switch jizz for macos port

--- daemon/lokinet.swift
+++ daemon/lokinet.swift
@@ -1,4 +1,3 @@
-
 // AppDelegateExtension.swift
 // lifed from yggdrasil network ios port
 //
@@ -16,47 +15,54 @@ class LokinetMain: NSObject {
 
     func runMain() {
         print("Starting up lokinet")
-        NETunnelProviderManager.loadAllFromPreferences { (savedManagers: [NETunnelProviderManager]?, error: Error?) in
-            if let error = error {
-                print(error)
-            }
+        let providerProtocol = NETunnelProviderProtocol()
+        providerProtocol.providerBundleIdentifier = self.lokinetComponent
+        providerProtocol.isEnabled = true
+        self.vpnManager.protocolConfiguration = providerProtocol
 
-            if let savedManagers = savedManagers {
-                for manager in savedManagers {
-                    if (manager.protocolConfiguration as? NETunnelProviderProtocol)?.providerBundleIdentifier == self.lokinetComponent {
-                        print("Found saved VPN Manager")
-                        self.vpnManager = manager
-                    }
+        self.vpnManager.saveToPreferences(completionHandler: {(error) -> Void in
+                if(error != nil) {
+                    print("Error saving to preferences")
+                } else {
+                    print("saved...")
+                    self.vpnManager.loadFromPreferences(completionHandler: { (error) in
+                        if (error != nil) {
+                            print("Error loading from preferences")
+                        } else {
+                            do {
+                                print("Trying to start")
+                                self.initializeConnectionObserver()
+                                try self.vpnManager.connection.startVPNTunnel()
+                            } catch let error as NSError {
+                                print(error)
+                            } catch {
+                                print("There was a fatal error")
+                            }
+                        }
+                    })
                 }
-            }
+                                          })
+        print("wait...")
+    }
 
-            self.vpnManager.loadFromPreferences(completionHandler: { (error: Error?) in
-                if let error = error {
-                    print(error)
-                }
+    func initializeConnectionObserver () {
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.NEVPNStatusDidChange, object: self.vpnManager.connection, queue: OperationQueue.main) { (notification) -> Void in
 
-                self.vpnManager.localizedDescription = "Lokinet"
-                self.vpnManager.isEnabled = true
-            })
-        }
-
-        do
-        {
-            try self.vpnManager.connection.startVPNTunnel()
-        }
-        catch {
-            switch self.vpnManager.connection.status {
-            case NEVPNStatus.invalid:
-                print("lokinet connection invalid")
-            default:
-                print("lokinet did not start")
+            if self.vpnManager.connection.status == .invalid {
+                print("VPN configuration is invalid")
+            } else if self.vpnManager.connection.status == .disconnected {
+                print("VPN is disconnected.")
+            } else if self.vpnManager.connection.status == .connecting {
+                print("VPN is connecting...")
+            } else if self.vpnManager.connection.status == .reasserting {
+                print("VPN is reasserting...")
+            } else if self.vpnManager.connection.status == .disconnecting {
+                print("VPN is disconnecting...")
             }
         }
-        
     }
 }
 
 
 let lokinet = LokinetMain()
 lokinet.runMain()
-
